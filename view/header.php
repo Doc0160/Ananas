d7a300852faacaaebf839305773ff251d1bd7670
Tristan Magniez <tristan.magniez@viacesi.fr>
2017-04-12 19:44:59 +0200

    12/04/2017-19:44:59,35

--- view/header.php
+++ view/header.php
@@ -22,6 +22,11 @@
          .main {
              flex: 1 0 auto;
          }
+
+         nav, .page-footer {
+             background: #c2242a;
+         }
+         
         </style>
     </head>
 
@@ -48,17 +53,25 @@
         </nav>
         
         <?php if(!empty($_SESSION["username"])) { ?>
-        <ul id="slide-out" class="side-nav">
-            <li><div class="userView">
-                <div class="background">
-                    <img src="http://materializecss.com/images/office.jpg">
-                </div>
-                <a href="#!user"><img class="circle" src="http://materializecss.com/images/yuna.jpg"></a>
-                <a href="#!name"><span class="white-text name">John Doe</span></a>
-                <a href="#!email"><span class="white-text email">jdandturk@gmail.com</span></a>
-            </div></li>
-            <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
-            <li><a href="#!">Second Link</a></li>
+            <ul id="slide-out" class="side-nav">
+                <li>
+                    <div class="userView">
+                        <div class="background">
+                            <img src="http://materializecss.com/images/office.jpg">
+                        </div>
+                        <a href="#!user">
+                            <img class="circle" src="http://materializecss.com/images/yuna.jpg">
+                        </a>
+                        <a href="#!name">
+                            <span class="white-text name">John Doe</span>
+                        </a>
+                        <a href="#!email">
+                            <span class="white-text email">jdandturk@gmail.com</span>
+                        </a>
+                    </div>
+                </li>
+                <li><a href="#!"><i class="material-icons">cloud</i>First Link With Icon</a></li>
+                <li><a href="#!">Second Link</a></li>
             <li><div class="divider"></div></li>
             <li><a class="subheader">Subheader</a></li>
             <li><a class="waves-effect" href="#!">Third Link With Waves</a></li>
