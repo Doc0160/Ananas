9325b50b555b6767ce24741d468cb0e383cbfc25
Tristan Magniez <tristan.magniez@viacesi.fr>
2017-04-13 04:19:43 +0200

    13/04/2017- 4:19:43,67

--- ananas.sql
+++ ananas.sql
@@ -11,94 +11,6 @@ DROP DATABASE IF EXISTS ananas;
 CREATE DATABASE IF NOT EXISTS ananas;
 USE ananas;
 
-/* activity related tables */
-
-CREATE TABLE `activity` (
-`id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-`date` timestamp,
-`regular` int(11),
-
-PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-CREATE TABLE `activity_inscription` (
-`id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-`id_activity` int(11) UNSIGNED NOT NULL,
-`id_user` int(11) UNSIGNED NOT NULL,
-
-PRIMARY KEY (`id`),
-CONSTRAINT `a_i_ibfk_1` FOREIGN KEY (`id_activity`) REFERENCES activity(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-CONSTRAINT `a_i_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES user(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-CREATE TABLE `activity_suggestion` (
-`id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-`name` varchar(255) DEFAULT NULL,
-`description` varchar(2048) NOT NULL,
-
-PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-CREATE TABLE `activity_vote` (
-`id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-`id_activity` int(11) UNSIGNED NOT NULL,
-
-PRIMARY KEY (`id`),
-CONSTRAINT `a_v_ibfk_1` FOREIGN KEY (`id_activity`) REFERENCES activity(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-
-/* goodies related tables */
-
-CREATE TABLE `goodies` (
-`id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-`price` float NOT NULL,
-`name` varchar(255) NOT NULL,
-`picture` varchar(255),
-`description` varchar(2048) NOT NULL,
-
-PRIMARY KEY (`id`),
-CHECK (`price` > 0)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-CREATE TABLE `goodies_reservation` (
-`id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-`id_goodies` int(11) UNSIGNED NOT NULL,
-`id_user` int(11) UNSIGNED NOT NULL,
-
-PRIMARY KEY (`id`),
-CONSTRAINT `g_r_ibfk_1` FOREIGN KEY (`id_goodies`) REFERENCES goodies(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-CONSTRAINT `g_r_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES user(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-
-/* photo related tables */
-
-CREATE TABLE `photo_comment` (
-`id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-`id_photo` int(11) UNSIGNED NOT NULL,
-`comment` varchar(1024) NOT NULL,
-
-PRIMARY KEY (`id`),
-CONSTRAINT `p_c_ibfk_1` FOREIGN KEY (`id_photo`) REFERENCES photo(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-CREATE TABLE `photo_like` (
-`id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-`id_photo` int(11) UNSIGNED NOT NULL,
-
-PRIMARY KEY (`id`),
-CONSTRAINT `p_l_ibfk_1` FOREIGN KEY (`id_photo`) REFERENCES photo(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
-
-CREATE TABLE `photo` (
-`id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-`id_activity` int(11) UNSIGNED NOT NULL,
-`picture` varchar(255) NOT NULL,
-
-PRIMARY KEY (`id`),
-CONSTRAINT `p_ibfk_1` FOREIGN KEY (`id_activity`) REFERENCES activity(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
 
 /* user related tables */
@@ -109,7 +21,7 @@ CREATE TABLE `groupe` (
 `permissions` int(11) NOT NULL,
 
 PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO `groupe` (`name`, `permissions`)
 VALUES ('BDE', '9999999999');
@@ -130,7 +42,7 @@ CREATE TABLE `user` (
 
 PRIMARY KEY (`id`),
 CONSTRAINT`u_ibfk_1` FOREIGN KEY (`id_groupe`) REFERENCES groupe(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO `user` (`username`, `name`, `pass`, `email`, `id_groupe`)
 VALUES ('JPBDE', 'Jean-Paul BDE', '?', 'a@b.c1', 1);
@@ -139,6 +51,96 @@ VALUES ('JPCESI', 'Jack-Pierre CESI', '?', 'a@b.c2', 2);
 INSERT INTO `user` (`username`, `name`, `pass`, `email`, `id_groupe`)
 VALUES ('Peni', 'penisland', '?', 'a@b.c3', 3);
 
+
+/* activity related tables */
+
+CREATE TABLE `activity` (
+`id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+`date` timestamp,
+`regular` int(11),
+
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `activity_inscription` (
+`id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+`id_activity` int(11) UNSIGNED NOT NULL,
+`id_user` int(11) UNSIGNED NOT NULL,
+
+PRIMARY KEY (`id`),
+CONSTRAINT `a_i_ibfk_1` FOREIGN KEY (`id_activity`) REFERENCES activity(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+CONSTRAINT `a_i_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES user(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `activity_suggestion` (
+`id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+`name` varchar(255) DEFAULT NULL,
+`description` varchar(2048) NOT NULL,
+
+PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `activity_vote` (
+`id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+`id_activity` int(11) UNSIGNED NOT NULL,
+
+PRIMARY KEY (`id`),
+CONSTRAINT `a_v_ibfk_1` FOREIGN KEY (`id_activity`) REFERENCES activity(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+/* goodies related tables */
+
+CREATE TABLE `goodies` (
+`id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+`price` float NOT NULL,
+`name` varchar(255) NOT NULL,
+`picture` varchar(255),
+`description` varchar(2048) NOT NULL,
+
+PRIMARY KEY (`id`),
+CHECK (`price` > 0)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `goodies_reservation` (
+`id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+`id_goodies` int(11) UNSIGNED NOT NULL,
+`id_user` int(11) UNSIGNED NOT NULL,
+
+PRIMARY KEY (`id`),
+CONSTRAINT `g_r_ibfk_1` FOREIGN KEY (`id_goodies`) REFERENCES goodies(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+CONSTRAINT `g_r_ibfk_2` FOREIGN KEY (`id_user`) REFERENCES user(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+/* photo related tables */
+
+CREATE TABLE `photo` (
+`id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+`id_activity` int(11) UNSIGNED NOT NULL,
+`picture` varchar(255) NOT NULL,
+
+PRIMARY KEY (`id`),
+CONSTRAINT `p_ibfk_1` FOREIGN KEY (`id_activity`) REFERENCES activity(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `photo_comment` (
+`id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+`id_photo` int(11) UNSIGNED NOT NULL,
+`comment` varchar(1024) NOT NULL,
+
+PRIMARY KEY (`id`),
+CONSTRAINT `p_c_ibfk_1` FOREIGN KEY (`id_photo`) REFERENCES photo(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `photo_like` (
+`id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+`id_photo` int(11) UNSIGNED NOT NULL,
+
+PRIMARY KEY (`id`),
+CONSTRAINT `p_l_ibfk_1` FOREIGN KEY (`id_photo`) REFERENCES photo(`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
 /*
 CREATE TABLE `permission` (
 `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
@@ -146,7 +148,7 @@ CREATE TABLE `permission` (
 `value` int(11) NOT NULL UNIQUE,
 
 PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO `permission` (`name`, `value`) VALUES ('suggest an activity', '1');
 INSERT INTO `permission` (`name`, `value`) VALUES ('modify activity', '2');
