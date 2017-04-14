35fe75a813f04405fdf89c05115e7e9f25ed172b
Tristan Magniez <tristan.magniez@viacesi.fr>
2017-04-15 01:47:13 +0200

    15/04/2017- 1:47:13,74

--- build.bat
+++ build.bat
@@ -1,13 +1,17 @@
 @echo off
 echo.
-echo git status
+echo --- git status ---
 git status
 echo.
+echo --- git pull ---
 git pull
 echo.
+echo --- git add ---
 git add .
 echo.
+echo --- git commit ---
 git commit -m "%date%-%time%"
 echo.
+echo --- git push ---
 git push
 echo.
