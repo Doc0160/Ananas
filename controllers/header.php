c2a3cc604019b7d68f2e06fc1e8aa14311ed56e4
Tristan Magniez <tristan.magniez@viacesi.fr>
2017-04-14 10:59:33 +0200

    14/04/2017-10:59:33,28

--- controllers/header.php
+++ controllers/header.php
@@ -1,10 +1,12 @@
 <?php
-
+$user = [];
+if($session->has_data()) {
 $req = $data['database']->prepare('SELECT * FROM user WHERE id=:id');
 $id = $data['session']->id;
 $req->bindParam(':id', $id);
 $req->execute();
-$user = $req->fetch();
+    $user = $req->fetch();
+}
 
 $data['view']->display('header.php', $user);
 
