669fd109f860107db95b79f908f569415fdb117e
Tristan Magniez <tristan.magniez@viacesi.fr>
2017-04-14 05:35:09 +0200

    14/04/2017- 5:35:09,29

--- controllers/login.php
+++ controllers/login.php
@@ -5,7 +5,7 @@ if (!empty($_POST['email']) && !empty($_POST['pass']))
 	$email = filter_input(INPUT_POST, 'email', FILTER_SANITIZE_EMAIL);
 	$pass = hash("sha256", $_POST['pass'], false);
 
-	$records = $database->prepare('SELECT * FROM user WHERE email = :email AND pass = :pass');
+	$records = $database->prepare('SELECT user.*, groupe.permissions FROM user WHERE email = :email AND pass = :pass JOIN groupe ON user.id_groupe = groupe.id');
 	$records->bindParam(':email', $email, PDO::PARAM_STR);
 	$records->bindParam(':pass', $pass, PDO::PARAM_STR);
 	$records->execute();
@@ -16,6 +16,7 @@ if (!empty($_POST['email']) && !empty($_POST['pass']))
 		$session->email = $results["email"];
 		$session->username = $results["username"];
 		$session->id = $results["id"];
+		$session->permissions = $results["permissions"];
 		Router::redirect("/");
 	}
 	else
