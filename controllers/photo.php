6b013ddca7dbe7ac3aea9569c362193f32d37e09
Tristan Magniez <tristan.magniez@viacesi.fr>
2017-04-14 11:48:20 +0200

    14/04/2017-11:48:20,02

--- controllers/photo.php
+++ controllers/photo.php
@@ -1,7 +1,9 @@
 <?php
 
+$req = $data['database']->prepare('SELECT * FROM photo');
+$req->execute();
+$photos = $req->fetchAll();
 
-
-$data["view"]->display('photos.php', []);
+$data["view"]->display('photos.php', $photos);
 
 ?>
