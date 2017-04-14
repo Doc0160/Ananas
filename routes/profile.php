3ce09cd1ca224e7c51a795846d67da45ddbfb3f9
Tristan Magniez <tristan.magniez@viacesi.fr>
2017-04-14 11:21:20 +0200

    14/04/2017-11:21:20,70

--- routes/profile.php
+++ routes/profile.php
@@ -10,13 +10,13 @@ $router->get('/profile/', function() use ($view, $controller, $database){
 
 $router->post('/profile/', function() use ($database, $session) {
     $req = $database->prepare("UPDATE user SET avatar=:photo WHERE id=:id");
-    $photo = $_FILES['avatar']['name'];
+    $photo = time().$_FILES['avatar']['name'];
     $id = $session->id;
     $req->bindParam(":photo", $photo);
     $req->bindParam(":id", $id);
     var_dump($_FILES);
     move_uploaded_file($_FILES['avatar']['tmp_name'],
-                       BASEPATH.BASEIMAGE.'/'.$_FILES['avatar']['name']);
+                       BASEPATH.BASEIMAGE.'/'.time().$_FILES['avatar']['name']);
     $req->execute();
 });
 
