266957f943a62baa8ab0b57f2c8b57f02302219f
Tristan Magniez <tristan.magniez@viacesi.fr>
2017-04-14 12:10:07 +0200

    14/04/2017-12:10:07,23

--- views/carousel.php
+++ views/carousel.php
@@ -1,15 +1,20 @@
 
 <div class="row"></div>
 <div class="row">
-    <div class="carousel carousel-slider" data-indicators="true">
-        <?php
-        foreach ($data['images'] as $key => $value) {
-    	?>
-    	    <a class="carousel-item" href="<?php echo ROOTURL."/photos/"; ?>">
-                <img src="<?php echo ROOTURL."/images/".$value["picture"]; ?>">
-            </a>
-    	<?php
-        }
-        ?>
+    <div class="col s12">
+        <div class="carousel carousel-slider" data-indicators="true">
+            <?php
+            foreach ($data['images'] as $key => $value) {
+    	    ?>
+    	        <a class="carousel-item" href="<?php echo ROOTURL."/photos/"; ?>">
+                    <img src="<?php echo ROOTURL."/images/".$value["picture"]; ?>">
+                </a>
+    	    <?php
+            }
+            ?>
+        </div>
     </div>
+    <a class="col s12 btn" href="<?php echo ROOTURL."/photos/"; ?>">
+        Voir plus de photos
+    </a>
 </div>
