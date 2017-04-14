2bb92697641427b52764db19a9865d24775a4535
Tristan Magniez <tristan.magniez@viacesi.fr>
2017-04-14 16:54:03 +0200

    14/04/2017-16:54:03,60

--- routes/photos.php
+++ routes/photos.php
@@ -13,12 +13,22 @@ $router->post('/photos/', function() use ($router, $session, $database) {
     //$router->redirect("/");
     
     $id = (int)$session->id;
-    $id_activity = '';
+    $id_activity = (int)$_POST['activity'];
 
     $req = $database->prepare("INSERT INTO photo (id_activity, picture) VALUES (:id_activity, :photo)");
     $photo = time().$_FILES['photo']['name'];
     $req->bindParam(":photo", $photo);
     $req->bindParam(":id_activity", $id_activity);
+
+    if(is_uploaded_file($_FILES['photo']['tmp_name'])) {
+        if(move_uploaded_file($_FILES['photo']['tmp_name'],
+                              BASEPATH.BASEIMAGE.'/'.
+                              time().$_FILES['photo']['name']))
+        {
+            $req->execute();
+            $router->redirect("/");
+        }
+    }
     
     var_dump($_FILES);
     var_dump($_POST);
