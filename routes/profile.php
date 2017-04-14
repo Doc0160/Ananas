4934cf4fb365ad3bdb429854b22b933f776ab153
Tristan Magniez <tristan.magniez@viacesi.fr>
2017-04-14 11:30:02 +0200

    14/04/2017-11:30:02,75

--- routes/profile.php
+++ routes/profile.php
@@ -8,16 +8,17 @@ $router->get('/profile/', function() use ($view, $controller, $database){
     $view->display('footer.php');
 });
 
-$router->post('/profile/', function() use ($database, $session) {
+$router->post('/profile/', function() use ($router, $database, $session) {
     $req = $database->prepare("UPDATE user SET avatar=:photo WHERE id=:id");
     $photo = time().$_FILES['avatar']['name'];
     $id = $session->id;
     $req->bindParam(":photo", $photo);
     $req->bindParam(":id", $id);
-    var_dump($_FILES);
-    move_uploaded_file($_FILES['avatar']['tmp_name'],
-                       BASEPATH.BASEIMAGE.'/'.time().$_FILES['avatar']['name']);
-    $req->execute();
+    if(move_uploaded_file($_FILES['avatar']['tmp_name'],
+                       BASEPATH.BASEIMAGE.'/'.time().$_FILES['avatar']['name'])) {
+        $req->execute();
+    }
+    $router->redirect("/");
 });
 
 ?>
