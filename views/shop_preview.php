b5c97d061d389be23f2c1e1570096cf1db146b6f
ZacOnly <thomas.vaucois@viacesi.fr>
2017-04-14 13:05:57 +0200

    14/04/2017-13:05:56,96

---
+++ views/shop_preview.php
@@ -0,0 +1,16 @@
+<div class="row">
+        <div class="col s12 m4 l2">
+            <div class="card hoverable">
+                <div class="card-image">
+                    <img src="<?php echo $picture_1; ?>">
+                    <span class="card-title"></span>
+                </div>
+                <div class="card-content">
+                    <p></p>
+                </div>
+                <div class="card-action">
+                    <a href="#">Voir l'activité correspondante</a>
+                </div>
+            </div>
+        </div>
+</div>
