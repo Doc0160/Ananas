373c7a0e0d8793af4d5ee3a394b6022276c1a6a0
ZacOnly <thomas.vaucois@viacesi.fr>
2017-04-14 10:51:25 +0200

    14/04/2017-10:51:25,23

--- views/news.php
+++ views/news.php
@@ -1,6 +1,7 @@
 <?php
-    $max_length = substr($data[0]['description'], 0, 100) . ('...');
-?>+ 
+    $article_1 = substr($data[0]['description'], 0, 500) . ('...');
+    $article_2 = substr($data[1]['description'], 0, 500) . ('...');
+?>
 
 <div class="row">
         <div class="col l6 m12">
@@ -10,7 +11,7 @@
                     <span class="card-title"><?php echo $data[0]['name'] ; ?></span>
                 </div>
                 <div class="card-content">
-                    <p><?php echo $max_length; ?></p>
+                    <p><?php echo $article_1; ?></p>
                 </div>
                 <div class="card-action">
                     <a href="#">Voir l'article complet</a>
@@ -21,12 +22,11 @@
         <div class="col l6 m12">
             <div class="card hoverable">
                 <div class="card-image">
-                    <img src="<?php echo ROOTURL."/images/".$value["picture"]; ?>">
-                    <span class="card-title">Card Title</span>
+                    <img src="http://materializecss.com/images/sample-1.jpg">
+                    <span class="card-title"><?php echo $data[1]['name'] ; ?></span>
                 </div>
                 <div class="card-content">
-                    <p>I am a very simple card. I am good at containing small bits of information.
-                       I am convenient because I require little markup to use effectively.</p>
+                    <p><?php echo $article_2; ?></p>
                 </div>
                 <div class="card-action">
                     <a href="#">Voir l'article complet</a>
