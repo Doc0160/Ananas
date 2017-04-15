f9b7c364ed927763ea3c7f11b3afa6785ee6b92e
Tristan Magniez <tristan.magniez@viacesi.fr>
2017-04-15 04:57:27 +0200

    15/04/2017- 4:57:27,94

--- views/news.php
+++ views/news.php
@@ -4,6 +4,9 @@
         if (empty($v['picture'])){
             $v['picture'] = ROOTURL."/logo.png";
         }
+        if (strlen($v['description']) > 400){
+            $v['description'] = substr($v['description'], 0, 400) . ('...');
+        }
     ?>
     <div class="col l6 m12">
         <div class="card hoverable">
@@ -12,7 +15,7 @@
                 <span class="card-title"><?php echo $v['name'] ; ?></span>
             </div>
             <div class="card-content">
-                <p><?php echo $article_1; ?></p>
+                <p><?php echo $v['description']; ?></p>
             </div>
             <div class="card-action">
                 <a href="<?php echo ROOTURL."/activities/"; ?>">Voir l'activité correspondante</a>
