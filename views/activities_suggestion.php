f755bc7306ba251f36353e532619991f8067c23b
Thomas VAUCOIS <Thomas VAUCOIS>
2017-04-19 15:40:29 +0200

    19/04/2017-15:40:29,18

---
+++ views/activities_suggestion.php
@@ -0,0 +1,37 @@
+<div class="row">
+    <div class="col s12">
+        <div class="card hoverable">
+            <div class="card">
+                <div class="card-content">
+                    <span class="card-title">Votez pour les prochaines activités !</span>
+                    <table>
+                        <thead>
+                            <tr>
+                                <th><i class="material-icons">list</i>Activité</th>
+                                <th><i class="material-icons">alarm</i>Date</th>
+                                <th><i class="material-icons">done</i>Vote</th>
+                            </tr>
+                        </thead>
+
+                        <tbody>
+                        <?php
+                        foreach ($data['activity'] as $key => $value) {
+                        ?>
+                            <tr>
+                                <td><?php echo $value['name']; ?></td>
+                                <td><?php echo $value['date'] ?></td>
+                                <td><a href="#" class="btn"><i class="material-icons">send</i></a></td>
+                            </tr>
+                        <?php
+                        }
+                        ?>
+                        </tbody>
+                    </table>
+                        <div class="card-action">
+                            <a href="<?php echo ROOTURL."/activities/"; ?>">Voir toutes les activités</a>
+                        </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
