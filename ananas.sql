7d117caf634f8a9bedc84e7650aa10ea5317ca5d
Tristan Magniez <tristan.magniez@viacesi.fr>
2017-04-19 14:02:21 +0200

    19/04/2017-14:02:21,79

--- ananas.sql
+++ ananas.sql
@@ -111,7 +111,7 @@ INSERT INTO `goodies` (`price`, `name`, `description`, `picture`)
 VALUES (300.1, 'Logo Exia Reims', 'Une coupe en plastique de la forme de logo snkjfdkdsksbkfjbskjbfkjbsdkjbfkhbskhfbkshbkfhbkshbfkhbskhfbkhsbdkh', NULL),
 (500.4, 'TRUC', 'JE DECRIS UN TRUC', NULL),
 (500.4, 'Bucal', 'mumumu', NULL),
-(500.4, 'Casquette EXIA', 'ceci est un article de qualité', 'casquette.jpg');
+(500.4, 'Casquette EXIA', 'ceci est un article de qualité', 'casquette.jpg'),
 (500.4, 'Gourde EXIA', 'ceci est un article de qualité', 'gourde.jpg');
 
 CREATE TABLE `goodies_reservation` (
