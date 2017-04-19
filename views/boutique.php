6667702054afe35836b46fcc0cc7cd24768b56ed
Tristan Magniez <tristan.magniez@viacesi.fr>
2017-04-19 13:41:32 +0200

    19/04/2017-13:41:32,26

---
+++ views/boutique.php
@@ -0,0 +1,184 @@
+
+<div class="container"> 
+    <div class="row">
+        <div class="col s12">
+
+
+            <!-- boutique tuiles -->
+            
+            <div class="col s12 m4 l4">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="C:\Users\Etienne Dumais\Ananas\maquette\boutique image\casquette.jpg">
+                    </div>
+                    <div class="card-content">
+                        <span class="title"> <h5>Casquette exia</h5></span>
+                        <p>ceci est un article de qualité</p>
+                    </div>
+                    <div class="card-action">
+                        <span class="price"><h3>10€</h3></span>
+                        <a href="#">commander</a>
+                        
+                    </div>
+                </div>
+            </div>
+
+            <div class="col s12 m4 l4">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="C:\Users\Etienne Dumais\Ananas\maquette\boutique image\gourde.jpg">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title"><h5>Gourde Exia</h5></span>
+                        <p>ceci est un article de qualité</p>
+                    </div>
+                    <div class="card-action">
+                        <span class="price"><h3>100€</h3></span>
+                        <a href="#">commander</a>
+                        
+                    </div>
+                </div>
+            </div>
+
+            <div class="col s12 m4 l4">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="C:\Users\Etienne Dumais\Ananas\maquette\boutique image\pull.jpg">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title"><h5>Pull Exia</h5></span>
+                        <p>ceci est un article de qualité</p>
+                    </div>
+                    <div class="card-action">
+                        <span class="price"><h3>1000€</h3></span>
+                        <a href="#">commander</a>
+                        
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<div class="container"> 
+    <div class="row">
+        <div class="col s12">
+
+            
+            <div class="col s12 m4 l4">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="C:\Users\Etienne Dumais\Ananas\maquette\boutique image\sac.jpg">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title"><h5>Sac Exia</h5></span>
+                        <p>ceci est un article de qualité</p>
+                    </div>
+                    <div class="card-action">
+                        <span class="price"><h3>10€</h3></span>
+                        <a href="#">commander</a>
+                        
+                    </div>
+                </div>
+            </div>
+
+            <div class="col s12 m4 l4">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="C:\Users\Etienne Dumais\Ananas\maquette\boutique image\kebab.jpg">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title"><h5>kebab Exia</h5></span>
+                        <p>ceci est un article de qualité</p>
+                    </div>
+                    <div class="card-action">
+                        <span class="price"><h3>5€</h3></span>
+                        <a href="#">commander</a>
+                        
+                    </div>
+                </div>
+            </div>
+
+            <div class="col s12 m4 l4">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="C:\Users\Etienne Dumais\Ananas\maquette\boutique image\siphano.jpg">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title"><h5>T-shirt siphano</h5></span>
+                        <p>ceci est un article de qualité</p>
+                    </div>
+                    <div class="card-action">
+                        <span class="price"><h3>1€ svp</h3></span>
+                        <a href="#">commander</a>
+                        
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<div class="container"> 
+    <div class="row">
+        <div class="col s12">
+
+            
+            <div class="col s12 m4 l4">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="C:\Users\Etienne Dumais\Ananas\maquette\boutique image\exia.jpg">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title"><h5>Ton Année à l'Exia</h5></span>
+                        <p>ceci est un article </p>
+                    </div>
+                    <div class="card-action">
+                        <span class="price"><h3>5500€</h3></span>
+                        <a href="#">commander</a>
+                        
+                    </div>
+                </div>
+            </div>
+
+            <div class="col s12 m4 l4">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="C:\Users\Etienne Dumais\Ananas\maquette\boutique image\deco.jpg">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title"><h5>le trophée Exia</h5></span>
+                        <p>dimensions :<br> 10cm x 5.5cm</p>
+                    </div>
+                    <div class="card-action">
+                        <span class="price"><h3>25€</h3></span>
+                        <a href="#">commander</a>
+                        
+                    </div>
+                </div>
+            </div>
+
+            <div class="col s12 m4 l4">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="C:\Users\Etienne Dumais\Ananas\maquette\boutique image\stage.jpg">
+                    </div>
+                    <div class="card-content">
+                        <span class="card-title"><h5>stage en entreprise</h5></span>
+                        <p>ceci est un article de qualité</p>
+                    </div>
+                    <div class="card-action">
+                        <span class="price"><h4>non disponible</h4></span>
+                        <a href="#">commander</a>
+                        
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+
+<!-- fin boutique tuiles -->
