228e76e30d2d3b02531a84231070cf139eea42ac
Tristan Magniez <tristan.magniez@viacesi.fr>
2017-04-20 16:09:32 +0200

    20/04/2017-16:09:32,86

--- public/index.php
+++ public/index.php
@@ -39,7 +39,8 @@ $do_header = function() use($controller, $database, $session) {
     ]);
 };
 
-$router->add('/', function() use ($do_header, $view, $controller, $session, $database, $cookie){
+$router->add('/', function()
+    use ($do_header, $view, $controller, $session, $database, $cookie){
     $do_header();
     $perm = (isset($session->permissions) ? $session->permissions : DEFAULT_PERMISSION_UNKNOWN);
     if(BitField::has($perm, PERMISSION_READ_PHOTO)) {
