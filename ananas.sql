2bb6672ecad29109bb097d9c21fcda94a31d58dc
Tristan Magniez <tristan.magniez@viacesi.fr>
2017-04-20 19:37:03 +0200

    20/04/2017-19:37:03,08

--- ananas.sql
+++ ananas.sql
@@ -63,7 +63,7 @@ CHECK (`price` > 0)
 
 INSERT INTO `activity` (`date`, `regular`, `prix`, `name`, `description`)
 VALUES (FROM_UNIXTIME(1494112249), NULL, 10.1, 'LAN', 'LALAN'),
-VALUES (FROM_UNIXTIME(1494112270), NULL, 10.1, 'LAN', 'LALAN'),
+VALUES (FROM_UNIXTIME(1494112270), NULL, 10.1, 'LAN2', 'francis LALAN'),
 (NULL, NULL, 10.1, 'PENISLAND', 'Many companies specialize in custom pens in bulk orders of hundreds or thousands of units, but where do you go if you want just one or two customized pens for that special gift? You come to Pen Island, where we are passionate about creating the exact pen you want, a pen that is as unique as you are. You do not need to be an design expert to own a one-of-a-kind pen, our team will work with you to find a design uniquely yours. For those do not have the time to design their own pens we offer our pre-designed line of pens, some of which can be additionally customized. Whether you are looking for a thin white EZ-Grip pen, a petite yellowheart wood pen, or a thick dark mahogany pen we have just the one for you. Our pens are available in several custom sizes. Once we built a pen so large that we had difficulty finding a box it would fit in. We Specialize In Wood We have been hand-crafting wooden pens for nearly three decades and our designs have won multiple awards. From single pens to bulk orders, virgin timber or reclaimed barn wood. Visit our wood page for further information.');
 
 CREATE TABLE `activity_inscription` (
